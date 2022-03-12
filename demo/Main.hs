c295f53af7f4f2d37944806a19dfb1483714c07c
thma <gH1710>
2022-03-12 19:56:04 +0100

    work on the demo

--- demo/Main.hs
+++ demo/Main.hs
@@ -2,8 +2,12 @@ module Main where
 
 import Data.BigDecimal
 
+bd :: String -> BigDecimal 
+bd = read
+
+main :: IO ()
 main = do
-  let a = read "3.1415926" :: BigDecimal
+  let a = bd "3.1415926"
       b = bigDecimal 31415926 7
   print $ 100 * a
   print $ 100 * b
