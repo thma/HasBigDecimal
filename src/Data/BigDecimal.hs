401c41ccb77bf8e88bc5cd51981c7ea9b21ead7d
Michael Lan <michaellan202@gmail.com>
2022-08-23 07:48:33 -0700

    fix precision() on pathological inputs
    * certain inputs like 9999..9999 cause logBase to round up incorrectly
    * cannot really optimize because, e.g., `logBase 10 $ 10 ^ 20 - 1` is
      already 20 when it should be 19.99999 etc.

--- src/Data/BigDecimal.hs
+++ src/Data/BigDecimal.hs
@@ -171,11 +171,8 @@ matchScales (a@(BigDecimal integerA scaleA), b@(BigDecimal integerB scaleB))
 -- | returns the number of digits of a BigDecimal
 precision :: BigDecimal -> Natural
 precision 0                  = 1
-precision (BigDecimal val _)
-  | val < (10 ^ 308) = 1 + floor (logBase 10 $ abs $ fromInteger val)
-  | otherwise = go 1 $ abs val
-    where
-        go ds n = if n >= 10 then go (ds + 1) (n `div` 10) else ds
+precision (BigDecimal val _) = go 1 $ abs val
+  where go ds n = if n >= 10 then go (ds + 1) (n `div` 10) else ds
 
 -- | removes trailing zeros from a BigDecimals intValue by decreasing the scale
 trim :: Natural -> BigDecimal -> BigDecimal
